8c4d9077dd1eb1ac3ae310df25d0b4405cef3580
gil-666 <143546119+gil-666@users.noreply.github.com>
2024-11-24 19:59:32 -0600

    carousel live data

---
+++ app/src/main/java/com/example/vsthetics/HomeImageCarouselAdapter.java
@@ -0,0 +1,95 @@
+package com.example.vsthetics;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.vsthetics.Model.Servicios;
+import com.example.vsthetics.ui.home.HomeFragment;
+
+import java.util.List;
+
+public class HomeImageCarouselAdapter extends RecyclerView.Adapter<HomeImageCarouselAdapter.ViewHolder> {
+    HomeFragment context;
+    private List<Servicios> serviciosList;
+    OnItemClickListener onItemClickListener;
+
+    public HomeImageCarouselAdapter(HomeFragment context, List<Servicios> serviciosList) {
+        this.context = context;
+        this.serviciosList = serviciosList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context.getContext()).inflate(R.layout.image_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Servicios servicio = serviciosList.get(position);
+        String base64Image = servicio.getFoto();
+        if (base64Image != null && !base64Image.isEmpty()) {
+            try {
+                byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
+                Bitmap decodedBitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                holder.imageView.setImageBitmap(decodedBitmap);
+            } catch (IllegalArgumentException e) {
+                Log.e("ImageDecodeError", "Fallo decodificar la imagen", e);
+                holder.imageView.setImageResource(0);
+            }
+        } else {
+            holder.imageView.setImageResource(0); //en blanco
+        }
+        holder.tvServTitulo.setText(
+                servicio.getNombre() != null ? servicio.getNombre() : "Nombre no disponible"
+        );
+        holder.tvServSub.setText(
+                servicio.getDescripcion() != null ? servicio.getDescripcion() : "Descripción no disponible"
+        );
+
+        holder.itemView.setOnClickListener(v -> {
+            if (onItemClickListener != null) {
+                onItemClickListener.onClick(holder.imageView, servicio);
+
+            }
+        });
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return serviciosList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView tvServTitulo, tvServSub;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvServTitulo = itemView.findViewById(R.id.tvServicioTitulo);
+            tvServSub = itemView.findViewById(R.id.tvServicioSubtitulo);
+            imageView = itemView.findViewById(R.id.list_item_image);
+        }
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public interface OnItemClickListener {
+        void onClick(ImageView imageView, Servicios servicio);
+    }
+}
--- app/src/main/java/com/example/vsthetics/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/vsthetics/ui/home/HomeFragment.java
@@ -1,27 +1,31 @@
 package com.example.vsthetics.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.vsthetics.ImageAdapter;
-import com.example.vsthetics.MainActivity;
-import com.example.vsthetics.R;
+import com.example.vsthetics.HomeImageCarouselAdapter;
+import com.example.vsthetics.Model.Servicios;
 import com.example.vsthetics.databinding.FragmentHomeBinding;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
-
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FragmentHomeBinding binding;
+    private HomeImageCarouselAdapter adapter;
+    private List<Servicios> serviciosList = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -31,22 +35,11 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         RecyclerView recyclerView = binding.recycler;
-        ArrayList<String> arrayList = new ArrayList<>();
+        adapter = new HomeImageCarouselAdapter(HomeFragment.this, serviciosList);
 
-        //llenar imagenes, se usan solo las que estan en res/drawable y sin extension
-        arrayList.add("logo");
-        arrayList.add("rac");
-        arrayList.add("cat");
+        cargarServicios(recyclerView, adapter);
 
-        ImageAdapter adapter = new ImageAdapter(HomeFragment.this, arrayList);
-        recyclerView.setAdapter(adapter);
 
-        adapter.setOnItemClickListener(new ImageAdapter.OnItemClickListener() {
-            @Override
-            public void onClick(ImageView imageView, String path) {
-                //que hacer si hay clic
-            }
-        });
 //        final TextView textView = binding.textHome;
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
@@ -57,4 +50,50 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    public void cargarServicios(RecyclerView recyclerView, HomeImageCarouselAdapter adapter) {
+        db.collection("Servicios")
+                .addSnapshotListener((queryDocumentSnapshots, e) -> {
+                    if (e != null) {
+                        Log.e("FirestoreError", "Error al cargar servicios", e);
+                        return;
+                    }
+
+                    if (queryDocumentSnapshots != null) {
+                        serviciosList.clear();
+                        for (DocumentSnapshot document : queryDocumentSnapshots) {
+                            try {
+                                Servicios servicio = document.toObject(Servicios.class);
+                                servicio.setUid(document.getId());
+                                if (servicio != null) {
+                                    if (servicio.getNombre() == null) { // datos temporales si el Servicio todavia no se termina de agregar
+                                        servicio.setNombre("Agregando...");
+                                    }
+                                    if (servicio.getDescripcion() == null) {
+                                        servicio.setDescripcion("Por favor espere...");
+                                    }
+                                    if (servicio.getPrecio() == 0) {
+                                        servicio.setPrecio(0.0);
+                                    }
+                                    if (servicio.getDuracion() == 0) {
+                                        servicio.setDuracion(1);
+                                    }
+                                    serviciosList.add(servicio);
+                                }
+
+
+                            }catch (Exception ex){
+
+                            }
+
+                        }
+                        adapter.setOnItemClickListener((imageView, servicio) -> {
+                            Log.d("RecyclerView", "Clic item: " + servicio.getUid());
+                        });
+                        recyclerView.setAdapter(adapter);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+    }
 }
+
